789a93849dda5c7a8cead7a3370ad849e7ad2848
Rick Waldron <waldron.rick@gmail.com>
2011-11-22 16:41:26 -0500

    Initialize empty jQuery.browser compat plugin

---
+++ jquery.browser.js
@@ -0,0 +1,12 @@
+/*
+ * jQuery.browser Compatibility Plugin for jQuery 1.8
+ * By John Resig
+ * Dual licensed under MIT and GPL.
+ */
+
+(function( window, jQuery ) {
+
+
+
+
+})( this, this.jQuery );
